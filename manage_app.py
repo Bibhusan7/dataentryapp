b6b40cf20211d17d342381ef785223434cfc2ce8
Shikhar Adhikari <67640904+Bibhusan7@users.noreply.github.com>
2023-08-28 20:56:11 +0545

    Add files via upload

--- manage_app.py
+++ manage_app.py
@@ -17,7 +17,7 @@ root.title('Student App')
 Ttl = Label(root, text="Data Entry", font=("Arial", 18))
 Ttl.pack(padx=20,pady=20)
 
-AskLabel = Label(root, text="Add: Adds new detail\nView: Shows details\nSearch: Finds details of specific person\nDelete: Delete a data\nExit: Exits",
+AskLabel = Label(root, text="Add: Adds new detail\nView: Shows details\nSearch: Finds details of specific person\nUpdate: Update a data\nDelete: Delete a data\nExit: Exits",
                  font=("Arial", 13, 'italic'))
 AskLabel.pack()
 
@@ -26,8 +26,8 @@ def adding():
     new.geometry("550x450")
     new.title("Add data")
 
-    def values():
-        det = [name.get(), age.get(), address.get(), mark.get(), section.get()]
+    def values(event):
+        det = [name.get().capitalize(), age.get(), address.get(), mark.get(), section.get()]
         mycursor.execute(f"insert into students(name,age,address,mark,section)values('{det[0]}',{det[1]},'{det[2]}',{det[3]},'{det[4]}')")
         mydb.commit()
         new.destroy()
@@ -58,6 +58,8 @@ def adding():
 
     sub_btn = Button(new, text="Submit", width=10, font=("Arial", 10, 'bold'), command=values)
     sub_btn.pack(pady=2)
+    new.bind('<Return>', values)
+
 def viewing():
     new = Toplevel()
     new.geometry("400x300")
@@ -79,7 +81,7 @@ def searching():
     ttl1 = Label(new, text="Find Data", font=('Arial', 18))
     ttl1.pack(padx=20, pady=20)
 
-    def getdata():
+    def getdata(event):
         src = to_search.get().capitalize()
         print(src)
         mycursor.execute(f"select * from students where name='{src}'")
@@ -95,6 +97,7 @@ def searching():
 
     btn= Button(new, text="Submit", command=getdata)
     btn.pack()
+    new.bind('<Return>', getdata)
 def delete():
     new = Toplevel()
     new.geometry("400x300")
@@ -105,9 +108,12 @@ def delete():
     ttl1 = Label(new, text="Delete Data", font=('Arial', 18))
     ttl1.pack(padx=20, pady=20)
 
-    def getdat():
+    def getdat(event):
         dell = to_del.get().capitalize()
-        mycursor.execute(f"delete from students where name='{dell}'")
+        if dell == "All":
+            mycursor.execute("TRUNCATE TABLE students")
+        else:
+            mycursor.execute(f"delete from students where name='{dell}'")
 
 
     Label(new, text="Delete Name", font=('Arial', 15, 'bold')).pack()
@@ -116,8 +122,32 @@ def delete():
 
     btn = Button(new, text="Submit", command=getdat)
     btn.pack()
+    new.bind('<Return>', getdat)
+def update():
+    new = Toplevel()
+    new.geometry("400x300")
+    new.title("Update Data")
 
-def val():
+    ttl1 = Label(new, text="Update Data", font=('Arial', 18))
+    ttl1.pack(padx=20, pady=20)
+
+    def getupdate(event):
+        up_name = to_up.get().capitalize()
+        new_name = cng.get().capitalize()
+        mycursor.execute(f"update students set name='{new_name}' where name='{up_name}'")
+
+    Label(new, text="Update Data of user", font=('Arial', 15, 'bold')).pack()
+    to_up = Entry(new)
+    to_up.pack()
+
+    Label(new, text="New Name of user", font=('Arial', 15, 'bold')).pack()
+    cng = Entry(new)
+    cng.pack()
+
+    btn = Button(new, text="Submit", command=getupdate)
+    btn.pack()
+    new.bind('<Return>', getupdate)
+def val(event):
     user = ask.get()
     user = user.lower()
     if user== 'add':
@@ -129,6 +159,8 @@ def val():
         searching()
     elif user == 'delete':
         delete()
+    elif user == 'update':
+        update()
     elif user == 'exit':
         exit()
     else:
@@ -140,5 +172,6 @@ ask.pack(padx=10,pady=10)
 askBtn = Button(root, text="Submit",width=10, font=("Arial", 10, 'bold'),command=val)
 askBtn.pack(pady=2)
 
+root.bind('<Return>', val)
 
 root.mainloop()
