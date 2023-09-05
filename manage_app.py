276220d2eb23d8e594f374a53e2fd8f4995bcd2c
Shikhar Adhikari <67640904+Bibhusan7@users.noreply.github.com>
2023-09-05 20:07:41 +0545

    Update manage_app.py

--- manage_app.py
+++ manage_app.py
@@ -1,30 +1,17 @@
+#modules
+from tkinter import *
 import mysql.connector
 
+#connecting to database
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="",db="std_details")
-if mydb:
-    print("connected")
-else:
-    print("not connected")
-
 mycursor = mydb.cursor()
 
-from tkinter import *
-
-root = Tk()
-root.geometry("400x300")
-root.title('Student App')
-
-Ttl = Label(root, text="Data Entry", font=("Arial", 18))
-Ttl.pack(padx=20,pady=20)
-
-AskLabel = Label(root, text="Add: Adds new detail\nView: Shows details\nSearch: Finds details of specific person\nUpdate: Update a data\nDelete: Delete a data\nExit: Exits",
-                 font=("Arial", 13, 'italic'))
-AskLabel.pack()
-
+#app functions
 def adding():
     new = Toplevel()
     new.geometry("550x450")
     new.title("Add data")
+    new.iconphoto(False, p1)
 
     def values(event=None):
         det = [name.get().capitalize(), age.get(), address.get(), mark.get(), section.get()]
@@ -59,11 +46,11 @@ def adding():
     sub_btn = Button(new, text="Submit", width=10, font=("Arial", 10, 'bold'), command=values)
     sub_btn.pack(pady=2)
     new.bind('<Return>', values)
-
 def viewing():
     new = Toplevel()
     new.geometry("400x300")
     new.title("View data")
+    new.iconphoto(False, p1)
 
     ttl1 = Label(new, text="View Data", font=('Arial', 18))
     ttl1.pack(padx=20, pady=20)
@@ -77,6 +64,7 @@ def searching():
     new = Toplevel()
     new.geometry("400x300")
     new.title("Search Data")
+    new.iconphoto(False, p1)
 
     ttl1 = Label(new, text="Find Data", font=('Arial', 18))
     ttl1.pack(padx=20, pady=20)
@@ -90,6 +78,7 @@ def searching():
                 if src in i:
                     dt = Label(new, text=f"{i}", font=("Arial", 15))
                     dt.pack()
+        
 
     Label(new, text="Search Name", font=('Arial',15,'bold')).pack()
     to_search = Entry(new)
@@ -102,6 +91,7 @@ def delete():
     new = Toplevel()
     new.geometry("400x300")
     new.title("Delete Data")
+    new.iconphoto(False, p1)
 
 
 
@@ -112,8 +102,10 @@ def delete():
         dell = to_del.get().capitalize()
         if dell == "All":
             mycursor.execute("TRUNCATE TABLE students")
+            new.destroy()
         else:
             mycursor.execute(f"delete from students where name='{dell}'")
+            new.destroy()
 
 
     Label(new, text="Delete Name", font=('Arial', 15, 'bold')).pack()
@@ -127,6 +119,7 @@ def update():
     new = Toplevel()
     new.geometry("400x300")
     new.title("Update Data")
+    new.iconphoto(False, p1)
 
     ttl1 = Label(new, text="Update Data", font=('Arial', 18))
     ttl1.pack(padx=20, pady=20)
@@ -135,6 +128,7 @@ def update():
         up_name = to_up.get().capitalize()
         new_name = cng.get().capitalize()
         mycursor.execute(f"update students set name='{new_name}' where name='{up_name}'")
+        new.destroy()
 
     Label(new, text="Update Data of user", font=('Arial', 15, 'bold')).pack()
     to_up = Entry(new)
@@ -147,6 +141,8 @@ def update():
     btn = Button(new, text="Submit", command=getupdate)
     btn.pack()
     new.bind('<Return>', getupdate)
+
+#Getting user choice values from entry and then calling the needed function
 def val(event=None):
     user = ask.get()
     user = user.lower()
@@ -165,6 +161,20 @@ def val(event=None):
         exit()
     else:
         pass
+
+#main window elements
+root = Tk()
+root.geometry("400x300")
+root.title('Student App')
+p1 = PhotoImage(file = 'logo.png')
+root.iconphoto(False, p1)
+
+Ttl = Label(root, text="Data Entry", font=("Arial", 18))
+Ttl.pack(padx=20,pady=20)
+
+AskLabel = Label(root, text="Add: Adds new detail\nView: Shows details\nSearch: Finds details of specific person\nUpdate: Update a data\nDelete: Delete a data\nExit: Exits",
+                 font=("Arial", 13, 'italic'))
+AskLabel.pack()
 
 ask = Entry(root)
 ask.pack(padx=10,pady=10)
